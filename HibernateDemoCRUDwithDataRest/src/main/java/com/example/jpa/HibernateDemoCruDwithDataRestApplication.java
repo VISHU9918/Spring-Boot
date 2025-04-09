210922b6d3f8cb8ac43be6e64a525a0cad24fa38
VISHU9918 <vk9628117@gmail.com>
2025-04-09 13:14:18 +0530

    First Commit

---
+++ HibernateDemoCRUDwithDataRest/src/main/java/com/example/jpa/HibernateDemoCruDwithDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateDemoCruDwithDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateDemoCruDwithDataRestApplication.class, args);
+	}
+
+}
