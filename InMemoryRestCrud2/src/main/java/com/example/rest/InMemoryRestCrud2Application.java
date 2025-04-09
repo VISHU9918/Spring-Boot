7413fb8fb027bab95713d57fda087c1882fdcddb
VISHU9918 <vk9628117@gmail.com>
2025-04-09 13:17:56 +0530

    First Commit

---
+++ InMemoryRestCrud2/src/main/java/com/example/rest/InMemoryRestCrud2Application.java
@@ -0,0 +1,13 @@
+package com.example.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryRestCrud2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMemoryRestCrud2Application.class, args);
+	}
+
+}
